37a3055a31b5936730c319af7eded0d2622832a1
Corey Crooks <your-email@example.com>
2024-07-23 17:19:09 -0700

    Add files via upload  Upload for the Unit 2 database integration

---
+++ IT391 Advanced Software Development/Unit 10/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using IT391_Unit10.Services;
+using IT391_Unit10.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IT391_Unit10
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
