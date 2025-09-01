0163757d0d5ac79c0b0e80cb2c636c5410921cce
Corey Crooks <crookscorey@gmail.com>
2025-08-31 17:23:05 -0700

    Create AppShell.xaml.cs
    Added a codestate to IT391.

---
+++ IT391 Advanced Software Development/Unit 10/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using IT391_Unit10.ViewModels;
+using IT391_Unit10.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace IT391_Unit10
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
